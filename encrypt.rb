b3d0d7fda3bbaf246939d28e1b4d675dd4a5d1a4
malcolli <malcolli2185@gmail.com>
2022-09-21 09:08:27 -0400

    OTP Files

---
+++ encrypt.rb
@@ -0,0 +1,82 @@
+class Encryption
+	attr_accessor :starting_phrase, :one_time_phrase, :encrypted_message
+
+## Encryption class initializes phrase instance variable provided by user
+## Default state is an empty string
+	def initialize (phrase_to_encode="")
+		@starting_phrase = phrase_content(phrase_to_encode)
+		@one_time_phrase = ""
+		@encrypted_message = ""
+	end
+
+## phrase_content takes user input and tests if it is a string and
+## if it only contains a string with only A-Z input
+	def phrase_content(phrase_to_encode)
+		@starting_phrase = ""
+		phrase_valid = validate_input(phrase_to_encode)
+		if (phrase_valid)
+			@starting_phrase = phrase_to_encode.upcase
+		end
+		@starting_phrase
+	end
+
+	def validate_input(phrase_to_encode)
+		validate_state = false
+		if (phrase_to_encode.is_a?(String) && phrase_to_encode != "" )
+			phrase_to_encode.gsub!(/\s+/, "")
+			if(phrase_to_encode[/[a-zA-Z]+/] == phrase_to_encode)
+				@starting_phrase = phrase_to_encode.upcase
+			end
+		end
+		validate_state
+	end
+
+## Generates one-time pad!
+	def randomize_phrase
+		if(@starting_phrase.length)
+			for letter in  1..@starting_phrase.length
+				letter_roulet = (65 + rand(25)).chr
+				@one_time_phrase += letter_roulet
+			end
+		end
+		@one_time_phrase
+	end
+
+## Encrypts starting message with one time pad value
+	def encrypt_phrase
+		unencoded_array = @starting_phrase.split('').map{|v| v.ord - 65}
+		one_time_array  = @one_time_phrase.split('').map{|v| v.ord - 65}
+		unencoded_array.each_with_index do |starting_value, index|
+			encoding_value = starting_value + one_time_array[index] - 1
+			@encrypted_message += ((encoding_value % 26) + 65).chr
+		end
+	end
+
+	## Decrypts starting message with one time pad value and encrypted message
+	def decrypt_phrase(encrypted_message, one_time_pad)
+		@encrypted_message = encrypted_message
+		@one_time_phrase   = one_time_pad
+		encrypted_array    = @encrypted_message.split('').map{|v| v.ord - 65}
+		one_time_array     = @one_time_phrase.split('').map{|v| v.ord - 65}
+		@starting_phrase   = ""
+		encrypted_array.each_with_index do |encrypted_value, index|
+			decoding_value = encrypted_value - one_time_array[index] +1
+			if(decoding_value >= 0)
+				@starting_phrase += (decoding_value + 65 ).chr
+			else
+				@starting_phrase += ((decoding_value+26) + 65 ).chr
+			end
+		end
+	end
+
+	def output_values
+		if (@starting_phrase != "")
+			puts "Unencrypted: #{@starting_phrase}"
+			puts "Pad:         #{@one_time_phrase}"
+			puts "Encrypted:   #{@encrypted_message}"
+		else
+			puts "Your message is empty or contains unaccepted characters."
+		end
+	end
+
+end
---
+++ otp_encrypt.rb
@@ -0,0 +1,109 @@
+class OneTimePad
+	attr_accessor :starting_phrase, :one_time_phrase,:encrypted_message,
+								:starting_array, :one_time_array, :encrypted_array
+
+## Encryption class initializes phrase instance variable provided by user
+## Default state is an empty string
+	def initialize (phrase_to_encode="")
+		@starting_phrase = phrase_content(phrase_to_encode)
+		@starting_array ||= []
+		@one_time_phrase = ""
+		@one_time_array ||= []
+		@encrypted_message = ""
+		@encrypted_array ||= []
+	end
+
+## phrase_content takes user input and tests if it is a string that
+## contains only ASCII decimal values between 33-126
+## \ " and '  are excluded input types for the user.
+	def phrase_content(phrase_to_encode)
+		@starting_phrase = ""
+		phrase_valid = validate_input(phrase_to_encode)
+		if (phrase_valid)
+				@starting_array = phrase_to_encode.split()
+				@starting_phrase = phrase_to_encode
+		end
+		@starting_phrase
+	end
+
+#Boolean test for phrase validity - returns true or false
+	def validate_input(phrase_to_encode)
+		validate_state = false
+		valid_chars = /[A-Za-z0-9 \/_,\.\^\*\+\?\(\)\[\{\|\-\]\}~$#%!:;<>=`]+/
+		if (phrase_to_encode.is_a?(String) && phrase_to_encode != "" )
+			valid_content = phrase_to_encode.gsub(/\s+/, "")
+			if(valid_content[valid_chars] == valid_content)
+				validate_state = true
+			end
+		end
+		validate_state
+	end
+
+## Generates one-time pad!
+	def randomize_phrase
+		@starting_array.each do |word|
+			random_word = ""
+			for letter in  1..word.length
+				spin = 33 + rand(93)
+				letter_roulet = spin.chr
+				random_word += letter_roulet
+			end
+			@one_time_array << random_word
+		end
+		@one_time_phrase = @one_time_array.join(" ")
+	end
+
+## Encrypts starting message with one time pad value
+	def encrypt_phrase
+		@starting_array.each_with_index do |word, outer_index|
+			encrypted_word = ""
+			unencoded_array = word.split('').map{|v| v.ord - 33}
+			one_time_array  = @one_time_array[outer_index].split('').map{|v| v.ord - 33}
+			unencoded_array.each_with_index do |starting_value, inner_index|
+				encoding_value = starting_value + one_time_array[inner_index] - 1
+				encrypted_word += ((encoding_value % 94) + 33).chr
+			end
+			@encrypted_array << encrypted_word
+		end
+		@encrypted_message = @encrypted_array.join(" ")
+	end
+
+## Decrypts starting message with one time pad value and encrypted message
+	def decrypt_phrase(encrypted_message, one_time_pad)
+		@encrypted_message = encrypted_message
+		@encrypted_array = @encrypted_message.split()
+		@one_time_phrase   = one_time_pad
+		@one_time_array = @one_time_phrase.split()
+		@encrypted_array.each_with_index do |word, outer_index|
+			single_word_decryption(word, outer_index)
+		end
+		@starting_phrase = @starting_array.join(" ")
+	end
+
+##Decrypts word letter by letter
+	def single_word_decryption(word, outer_index)
+		encrypted_array = word.split('').map{|v| v.ord - 33}
+		one_time_array  = @one_time_array[outer_index].split('').map{|v| v.ord - 33}
+		decoded_word = ""
+		encrypted_array.each_with_index do |encrypted_value, inner_index|
+			decoding_value = encrypted_value - one_time_array[inner_index] +1
+			if(decoding_value >= 0)
+				decoded_word += (decoding_value + 33 ).chr
+			else
+				decoded_word += ((decoding_value+94) + 33 ).chr
+			end
+		end
+		@starting_array << decoded_word
+	end
+
+	def output_values
+		if (@starting_phrase != "")
+			puts "Unencrypted: #{@starting_phrase}"
+			puts "Pad:         #{@one_time_phrase}"
+			puts "Encrypted:   #{@encrypted_message}"
+		else
+			puts "Your message is empty or contains unaccepted characters."
+		end
+	end
+
+end
